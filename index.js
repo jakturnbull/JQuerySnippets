77c8599bf63c0349de05ec8fb63a4695b156c4f4
Jak-Turnbull <jak.turnbull1@gmail.com>
2022-09-02 23:45:02 +0100

    jquery test siote section finished

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
-  $("h1").addClass("big-title");
-  $("button")
+  $("button").click(function(){
+    $("h1").slideUp().slideDown().animate({opacity: 0.5});
+  });
+  $("body").keypress(function(event){
+
+    $("h1").text(event.key);
+  });
+  $("h1").on("mouseover", function(){
+    $("h1").css("color", "green");
+  })
+  $("button").html("<em>dont click</em>")
+  $("a").attr("href","https://www.bing.com")
 });
